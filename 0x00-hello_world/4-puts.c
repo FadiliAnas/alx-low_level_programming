92a0d2fa9d213a54cbc227ed1293723cd6c28c69
FadilAnas <anasfadili65@gmail.com>
2023-10-05 13:28:57 -0700

    learning C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main (void){
-	char my_string[] = "$"Programming is like building a multilingual puzzle\n";
+	char my_string[] = "\"Programming is like building a multilingual puzzle\n";
 	return = 0;
 }
