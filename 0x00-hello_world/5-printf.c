14a0ca5a9c3d9ed2eb7f11235d98695e75a8d8b8
FadilAnas <anasfadili65@gmail.com>
2023-10-05 14:20:34 -0700

    learning C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * this file print use the fonction printf
- * and return 0
- **/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
