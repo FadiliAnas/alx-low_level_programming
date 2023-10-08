d31ca50612b3113a86941674126b019be81db889
FadilAnas <anasfadili65@gmail.com>
2023-10-08 08:38:23 -0700

    C

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 97;
+	int b = 65;
+
+	while (a < 123)
+	{
+		putchar(a);
+		a++;
+	}
+	while (b < 100)
+        {
+                putchar(b);
+                b++;
+        }
+	putchar(10);
+	return (0);
+}
