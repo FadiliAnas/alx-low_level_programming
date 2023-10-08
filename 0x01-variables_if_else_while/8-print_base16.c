6f0c2e09b67817ad7c6b51df7e629ab1802bc77b
FadilAnas <anasfadili65@gmail.com>
2023-10-08 12:35:10 -0700

    C

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 48;
+	int i = 97;
+	while (a < 58)
+	{
+		putchar(a);
+		a++;
+	}
+	while (i < 103)
+        {
+                putchar(i);
+                i++;
+        }
+	
+	putchar(10);
+	return (0);
+}
