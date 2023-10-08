2aba977c6020a0f7f57ec5f72171de26b5366205
FadilAnas <anasfadili65@gmail.com>
2023-10-08 08:59:06 -0700

    C

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,23 +7,14 @@
 int main(void)
 {
 	int a = 97;
-	int b = 102;
-	int c = 114;
 
-	while (a < 101)
+	while (a < 123)
 	{
-		putchar(a);
-		a++;
-	}
-	while (b < 113)
-	{
-		putchar(a);
-		b++;
-	}
-	while (c < 123)
-	{
-		putchar(a);
-		c++;
+		if (a != 101 && a != 113)
+		{
+			putchar(a);
+			a++;
+		}
 	}
 	putchar(10);
 	return (0);
