bfc0eb0aaf8ab652188932bfae6aac7e34031056
FadilAnas <anasfadili65@gmail.com>
2023-10-10 13:29:39 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/* more headers goes there */
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+void print_alphabet_x10(void)
+{
+	int u;
+	int i = 97 ;
+
+	for (u = 0; u < 10; u++)
+	{
+		while (i < 123)
+        {
+                _putchar(i);
+                i++;
+        }
+        _putchar(10);
+	}		
+}
