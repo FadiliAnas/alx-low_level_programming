3006c4b5786196ef47ddeea4446d90f076ce4a5c
FadilAnas <anasfadili65@gmail.com>
2023-10-10 14:07:54 -0700

    louby

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/* more headers goes there */
+/**
+ * int _islower(int c); - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int _islower(int c);
+{
+	int c;
+
+
+	if ( c < 96  && c < 122 )
+	{
+		return(1);
+	}
+	else
+	{
+		return(0)
+	}
+}
