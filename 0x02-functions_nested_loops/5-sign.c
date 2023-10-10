f3a0cb49545915cc2d65c8f2afa666281293ab21
FadilAnas <anasfadili65@gmail.com>
2023-10-10 14:51:01 -0700

    louby

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/* more headers goes there */
+/**
+ * int _islower(int c); - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int print_sign(int n)
+{
+	if(n > 0)
+	{
+		return(1);
+	}
+	else if(n = 0)
+	{
+		return(0);
+	}
+	else if(n < 0)
+	{
+		return(-1)
+	}
+
+}
