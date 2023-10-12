83018e46b7920a0247b738ebe610bb3df989440f
FadilAnas <anasfadili65@gmail.com>
2023-10-12 14:14:57 -0700

    louby

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,16 +5,16 @@
  *Return: Always 0.
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
-        int i;
+	int i;
 
-        i = 48;
-        while (i < 58 && i != 50 && i != 53) 
-        {
-                _putchar(i);
-                i++;
-        }
-        _putchar(10);
+	i = 48;
+	while (i < 58 && i != 50 && i != 53)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar(10);
 }
 
