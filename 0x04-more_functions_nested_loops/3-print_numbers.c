b5847adf8d17e2221dfd1b6851a611fe9bbbd6d1
FadilAnas <anasfadili65@gmail.com>
2023-10-12 13:51:32 -0700

    louby

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_numbers - check the code
+ *
+ *Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 49;
+	while (i < 58)
+	{
+		_putchar(i);
+		i++;
+	}
+	return (0);
+}
