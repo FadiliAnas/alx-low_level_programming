f57f28b8ae3b7c8f769821cb4a25b3dfc2efc433
FadilAnas <anasfadili65@gmail.com>
2023-10-12 18:16:03 -0700

    louby

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square made of '#' characters
+ * @size: the size of the square
+ */
+void print_square(int size)
+{
+    int row, column;
+
+    if (size > 0)
+    {
+        for (row = 0; row < size; row++)
+        {
+            for (column = 0; column < size; column++)
+            {
+                _putchar('#');
+            }
+            _putchar('\n');
+        }
+    }
+    else
+    {
+        _putchar('\n');
+    }
+}
+
