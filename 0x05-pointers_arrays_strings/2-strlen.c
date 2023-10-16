69121499d36d1be6df4853ed186832d193364866
FadilAnas <anasfadili65@gmail.com>
2023-10-16 13:16:45 -0700

    louby

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,11 +1,15 @@
 #include "main.h"
-
+/**
+ * _strlen - returns the length of a string
+ * @s: is a parameter
+ * Return: int
+ */
 int _strlen(char *s)
 {
 	int i;
 
 	i = 0;
-	while ( *s != '\0')
+	while (*s != '\0')
 	{
 		i++;
 		s++;
