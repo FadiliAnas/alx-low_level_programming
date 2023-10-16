ebb0eb53434ce826d465af1d7c8c8233f413b0fe
FadilAnas <anasfadili65@gmail.com>
2023-10-16 13:40:17 -0700

    louby

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,17 @@
 #include "main.h"
-
+/**
+ * _puts - prints a string followed by a new line
+ * @str: is a parameter
+ * Retur: void
+ */
 void _puts(char *str)
 {
 
-        while (*str != '\0')
-        {
+	while (*str != '\0')
+	{
 		_putchar(*str);
-                str++;
+		str++;
 
-        }
+	}
 	_putchar(10);
 }
