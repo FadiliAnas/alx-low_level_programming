43d48b1d014344782a3e5005a829684cf3a8ad57
FadilAnas <anasfadili65@gmail.com>
2023-10-17 14:10:43 -0700

    louby

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,20 +3,18 @@
 
 void print_rev(char *s)
 {
-	int i; 
-	int x;
-	
+	int i;
+	int j;
+
 	i =0;
 	while(*s != '\0')
 	{
 		i++;
-		s++;
 	}
-	x = i;
-	while (x >= i) 
+	
+	for(j=i ;j >= 0 ;j--)
 	{
-		_putchar(*s);
-		s--;
-		i--;
+		_putchar(s[j]);
 	}
+	_putchar(10);
 }
