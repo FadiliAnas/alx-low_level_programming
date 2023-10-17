19e5c5a18f0857004a6372d8f67e2fb3f280e251
FadilAnas <anasfadili65@gmail.com>
2023-10-17 14:34:14 -0700

    louby

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,21 +1,25 @@
 #include "main.h"
-
+/**
+ *print_rev - check the code
+ * @s: parametre
+ * Return: Always 0.
+ */
 
 void print_rev(char *s)
 {
 	int i;
 	int j;
 	char *p;
-	
-	p = s; 
-	i =0;
-	while(*p != '\0')
+
+	p = s;
+	i = 0;
+	while (*p != '\0')
 	{
 		i++;
 		p++;
 	}
-	
-	for(j=i-1 ;j >= 0 ;j--)
+
+	for (j = i - 1 ; j >= 0 ; j--)
 	{
 		_putchar(s[j]);
 	}
