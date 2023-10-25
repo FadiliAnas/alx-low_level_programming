d772e12459027d97ade97bad82e2f5f766f38677
FadilAnas <anasfadili65@gmail.com>
2023-10-25 16:32:55 -0700

    louby

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - check the code
- * @s: parametrr
- * Return: void
+ * _strlen_recursion - returns length of a string
+ * @s: parameter
+ * Return: int
  */
-void _print_rev_recursion(char *s)
+int _strlen_recursion(char *s)
 {
 	if (!*s)
-		return;
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
